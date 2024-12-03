a61345d75ff279ea9b7f89eb7ac96d762c6d705b
Desk1002 <126067725+Desk1002@users.noreply.github.com>
2024-12-03 01:33:26 +0100

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,4 +1,4 @@
-!pip install plotly pandas numpy plotly os warnings datetime streamlit 
+pip install plotly pandas numpy plotly os warnings datetime streamlit 
 
 import streamlit as st
 import pandas as pd
